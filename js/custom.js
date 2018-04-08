5c304e892f763a1217cfb2458becaaafc60ec5fb
Mario <luix30lp@gmail.com>
2018-04-08 17:39:33 -0500

    Added section with carousel

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,23 @@ $(document).ready(function(){
 	//animate on scroll
 	new WOW().init();
 });
+/* Work */
+$(function(){
+	$('#work').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		gallery:{
+			enabled: true
+		}
+	});
+});
+/* team section */
+$(function(){
+	$("#team-members").owlCarousel({
+		items:3,
+		autoplay: true,
+		smarthSpeed: 700,
+		loop:true,
+		autoplayHoverPause: true
+	});
+});
