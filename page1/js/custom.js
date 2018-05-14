af1d7674a0e0bb3fa8961c43cbce397d363f6dce
Mario <luix30lp@gmail.com>
2018-05-14 15:48:36 -0500

    events now responsive

--- page1/js/custom.js
+++ page1/js/custom.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	$('.item').css("width",$( window ).width()),
 	$('.out').css("width",$( window ).width()),
 	$('.active').css("width",$( window ).width()),
-	console.log($(".transbox").height()),
+	console.log($(".col-event").height()),
 	$('.owl-carousel').owlCarousel({
 		loop:true,
 		padding:0,
