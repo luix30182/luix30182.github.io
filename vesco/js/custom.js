c7da85d76e407dd4cb7a9a2fac4b29630d1082dc
Mario <luix30lp@gmail.com>
2018-06-14 20:30:46 -0500

    vesco fixed

--- vesco/js/custom.js
+++ vesco/js/custom.js
@@ -1,107 +1,146 @@
-/*Services*/
-$(document).ready(function(){
-	//animate on scroll
-	new WOW().init();
-});
-/* Work */
-$(function(){
-	$('#work').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		gallery:{
-			enabled: true
-		}
-	});
-});
-/* team section */
-$(function(){
-	$("#team-members").owlCarousel({
-		items:3,
-		autoplay: true,
-		smarthSpeed: 700,
-		loop:true,
-		autoplayHoverPause: true,
-		responsive:{
-			0:{
-				items: 1
-			},
-			480: {
-				items: 2
-			},
-			768:{
-				items: 3
-			}
-		}
-	});
-});
-/* Testimonials*/
-$(function(){
-	$("#customers-testimonials").owlCarousel({
-		items:1,
-		autoplay: true,
-		smarthSpeed: 700,
-		loop:true,
-		autoplayHoverPause: true
-	});
-});
-/* counter */
-$(function(){
-	$('.counter').counterUp({
-		delay: 10,
-		time: 1000
-	});
-});
-/* clients */
-$(function(){
-	$("#clients-list").owlCarousel({
-		items:6,
-		autoplay: true,
-		smarthSpeed: 700,
-		loop:true,
-		autoplayHoverPause: true,
-		responsive:{
-			0:{
-				items: 1
-			},
-			480: {
-				items: 3
-			},
-			768:{
-				items: 5
-			},
-			992:{
-				items:6
-			}
+/*====================================================
+                        SERVICES
+====================================================*/
+$(function () {
 
+    // animate on scroll
+    new WOW().init();
+});
+
+/*====================================================
+                        WORK
+====================================================*/
+$(function () {
+
+    $("#work").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+
+    });
+});
+
+/*====================================================
+                        TEAM
+====================================================*/
+$(function () {
+
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+		responsive:{
+		  0: {
+			items: 1
+		  },
+		  480: {
+			items: 2
+		  },
+		  768: {
+			items: 3
+		  }
 		}
-	});
+    });
 });
-/* Navigation */
-$(function(){
-	$(window).scroll(function(){
-		if($(this).scrollTop()<50){
-			$('nav').removeClass("vesco-top-nav");
-			$('#back-to-top').fadeOut();
-		}else{
-			$('nav').addClass("vesco-top-nav");
-			$('#back-to-top').fadeIn();
+
+/*====================================================
+                        TESTIMONIALS
+====================================================*/
+$(function () {
+
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
+
+/*====================================================
+                        STATS
+====================================================*/
+$(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+});
+
+/*====================================================
+                        CLIENTS
+====================================================*/
+$(function () {
+
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+		responsive: {
+		  0: {
+			items: 1
+		  },
+		  480: {
+			items: 3
+		  },
+		  768: {
+			items: 5
+		  },
+		  992: {
+			items: 6
+		  }
 		}
-	});
+    });
 });
-/* smooth scroling */
-$(function(){
-	$('a.smooth-scroll').click(function(event){
-		event.preventDefault();
-		//get section Id
-		var section = $(this).attr("href");
-		$('html,body').animate({
-			scrollTop: $(section).offset().top-64
-		}, 1250,"easeInOutExpo");
-	});
+
+/*====================================================
+                        NAVIGATION
+====================================================*/
+// Show/Hide transparent black navigation
+$(function () {
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() < 50) {
+            // hide nav
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();
+
+        } else {
+            // show nav
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
 });
-// clise mobile menu
+// Smooth scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get/return id like #about, #work, #team and etc
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    });
+});
+
+// Close mobile menu on click
 $(function(){
-	$('.navbar-collapse ul li a').on("click touch",function(){
-		$('.navbar-toggle').click();
-	});
+    
+    $(".navbar-collapse ul li a").on("click touch", function(){
+       
+        $(".navbar-toggle").click();
+    });
 });
